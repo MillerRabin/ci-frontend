cb7373c0ef57f1577719f69b8742b5c75c6ed662
MillerRabin <millerrabin@raintech.su>
2018-09-04 20:37:02 +0300

    remove search and breadcrumb

--- scripts/applications/frontpage.js
+++ scripts/applications/frontpage.js
@@ -2,11 +2,9 @@ import loader from '/scripts/loader.js';
 import '/scripts/applications/projectList.js';
 import '/scripts/applications/projectConfigs.js';
 import '/scripts/applications/projectLogs.js';
-import '/scripts/applications/search.js';
-import '/scripts/applications/breadcrumbs.js';
 import location from '/scripts/services/location.js';
 
-loader.application('frontpage', ['projectList', 'projectConfigs', 'projectLogs', 'search', 'breadcrumbs', async () => {
+loader.application('frontpage', ['projectList', 'projectConfigs', 'projectLogs', async () => {
     function init() {
         return {
             showConfigs: false
