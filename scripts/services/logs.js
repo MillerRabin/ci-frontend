e76fa05520558417211c8ca895f6c3f2b106bc29
MillerRabin <millerrabin@raintech.su>
2018-09-07 14:46:25 +0300

    added support event_data null in logs

--- scripts/services/logs.js
+++ scripts/services/logs.js
@@ -31,6 +31,11 @@ async function get(data) {
         data: sData
     });
     return postgres.toArray(rData, (item) => {
+        if (item.event_data == null) {
+            item.project_name = 'not specified';
+            item.branch = 'not specified';
+            return;
+        }
         const repository = (item.event_data.request != null) ? item.event_data.request.repository : item.event_data.repository;
         item.project_name = (repository != null) ? repository.full_name :
                             (item.event_data.project != null) ? item.event_data.project.project_name :
